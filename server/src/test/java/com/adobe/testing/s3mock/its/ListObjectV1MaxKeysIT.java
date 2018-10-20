135233f2eed0340b911cc88dccc0549f70eac05d
Artem Prigoda <arteamon@gmail.com>
2018-10-20 12:13:38 +0200

    Honor max-keys parameter in List Objects v1 API
    The implementations of the List Objects V1 API doesn't honor the max-keys parameters of ListObjectsRequest. It would be nice if s3-mock supported it to reduce the disparity with the features provided by the actual S3 service.
    
    Docs: https://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGET.html

---
+++ server/src/test/java/com/adobe/testing/s3mock/its/ListObjectV1MaxKeysIT.java
@@ -0,0 +1,93 @@
+/*
+ *  Copyright 2017-2018 Adobe.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.adobe.testing.s3mock.its;
+
+import static org.junit.Assert.assertEquals;
+
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ListObjectV1MaxKeysIT extends S3TestBase {
+
+  private final String bucketName = "some-bucket";
+
+  @Before
+  @Override
+  public void prepareS3Client() {
+    super.prepareS3Client();
+    s3Client.createBucket(bucketName);
+    s3Client.putObject(bucketName, "a", "");
+    s3Client.putObject(bucketName, "b", "");
+  }
+
+  @Test
+  public void limitsAmountOfObjectsBasedOnMaxKeys() {
+    ListObjectsRequest request = new ListObjectsRequest().withBucketName(bucketName).withMaxKeys(1);
+    ObjectListing objectListing = s3Client.listObjects(request);
+
+    // This assertion fails. listObjects returns 2 objects instead of 1.
+    assertEquals(1, objectListing.getObjectSummaries().size());
+  }
+
+  @Test
+  public void returnsAllIfMaxKeysIsDefault() {
+    ListObjectsRequest request = new ListObjectsRequest().withBucketName(bucketName);
+    ObjectListing objectListing = s3Client.listObjects(request);
+
+    assertEquals(2, objectListing.getObjectSummaries().size());
+  }
+
+  @Test
+  public void returnsAllIfMaxKeysEqualToAmountOfObjects() {
+    ListObjectsRequest request = new ListObjectsRequest().withBucketName(bucketName)
+            .withMaxKeys(2);
+    ObjectListing objectListing = s3Client.listObjects(request);
+
+    assertEquals(2, objectListing.getObjectSummaries().size());
+  }
+
+  @Test
+  public void returnsAllIfMaxKeysMoreThanAmountOfObjects() {
+    ListObjectsRequest request = new ListObjectsRequest().withBucketName(bucketName)
+            .withMaxKeys(3);
+    ObjectListing objectListing = s3Client.listObjects(request);
+
+    assertEquals(2, objectListing.getObjectSummaries().size());
+  }
+
+  @Test
+  public void returnsNoOnbjectsIfMaxKeysIsZero() {
+    ListObjectsRequest request = new ListObjectsRequest().withBucketName(bucketName)
+            .withMaxKeys(0);
+    ObjectListing objectListing = s3Client.listObjects(request);
+
+    assertEquals(0, objectListing.getObjectSummaries().size());
+  }
+
+  @Test
+  public void returnsAllOnbjectsIfMaxKeysIsNegative() {
+    ListObjectsRequest request = new ListObjectsRequest().withBucketName(bucketName)
+            .withMaxKeys(-1);
+    ObjectListing objectListing = s3Client.listObjects(request);
+
+    // Apparently, the Amazon SDK rejects negative max keys, and by default it's 1000
+    assertEquals(2, objectListing.getObjectSummaries().size());
+  }
+}
