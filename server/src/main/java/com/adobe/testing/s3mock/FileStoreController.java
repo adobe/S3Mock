6269bed4fb0fe2357d28b48f60e24f7b54a814a2
alinpa <alin.pa@mobfox.com>
2019-02-18 18:06:40 +0100

    added the header

--- server/src/main/java/com/adobe/testing/s3mock/FileStoreController.java
+++ server/src/main/java/com/adobe/testing/s3mock/FileStoreController.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017-2018 Adobe.
+ *  Copyright 2017-2019 Adobe.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package com.adobe.testing.s3mock;
 
 import static com.adobe.testing.s3mock.util.BetterHeaders.COPY_SOURCE;
