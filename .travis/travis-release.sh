76bb2bb7061766d789bbf20ed448faa9eb17949b
Lee Clench <leeclench@outlook.com>
2019-10-02 17:44:10 +0100

    Neatened up some comments and documentation elements that were missing punctuation.

--- .travis/travis-release.sh
+++ .travis/travis-release.sh
@@ -7,15 +7,15 @@ if [ -z "$DOCKERHUB_PASSWORD" ]; then
   exit 1
 fi
 
-echo "Performing docker login"
+echo "Performing docker login..."
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
 
-echo "Importing GPG keys"
+echo "Importing GPG keys..."
 echo $GPG_SECRET_KEYS | base64 --decode | gpg --import
 echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust
 
 echo "Checkout master branch explicitly, as we run the release with a in detached head."
 git checkout -qf master;
 
-echo "Starting Maven release"
+echo "Starting Maven release..."
 mvn --settings ./.travis/settings.xml release:prepare release:perform
