ddf6edfb8e9f38ede3038f74766994fb91780166
Andreas Gudian <gudian@adobe.com>
2020-08-12 17:59:12 +0200

    Add git user config to github release script

--- .github/workflows/perform-release.sh
+++ .github/workflows/perform-release.sh
@@ -15,6 +15,8 @@ echo $GPG_SECRET_KEYS | base64 --decode | gpg --import --no-tty --batch --yes
 echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust --no-tty --batch --yes
 
 echo "Checkout master branch explicitly, as we run the release with a in detached head."
+git config user.email "adobe-bot@example.com"
+git config user.name "adobe-bot"
 git checkout -qf master;
 
 echo "Starting Maven release..."
