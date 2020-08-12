0564c6a1bada2c757be1119ae7a003b9e050013b
Andreas Gudian <gudian@adobe.com>
2020-08-12 19:13:38 +0200

    Rename master branch to main

--- .github/workflows/perform-release.sh
+++ .github/workflows/perform-release.sh
@@ -14,8 +14,8 @@ echo "Importing GPG keys..."
 echo $GPG_SECRET_KEYS | base64 --decode | gpg --import --no-tty --batch --yes
 echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust --no-tty --batch --yes
 
-echo "Checkout master branch explicitly, as we run the release with a in detached head."
-git config user.email "${X_GITHUB_USERNAME}@example.com"
+echo "Configure git for the release commits"
+git config user.email "Grp-opensourceoffice@adobe.com"
 git config user.name "${X_GITHUB_USERNAME}"
 
 echo "Starting Maven release..."
