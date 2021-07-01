19d18b28cda450e853d11b3e97c693d54296b3ea
Arne Franken <franken@adobe.com>
2021-07-01 22:20:13 +0200

    Update spring-boot to 2.3.12.RELEASE
    Also updates
    spring-security-oauth2 to 2.5.1.RELEASE
    jackson to 2.11.4
    These versions only work in tandem, so I'm updating all 3 in one commit.
    
    Added dummy "favicon.ico" controller. Spring Boot does not come with a
    default favicon any more.
    Spring MVC dropped the "charset" portion from the content-type header.

---
+++ server/src/main/java/com/adobe/testing/s3mock/FaviconController.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2017-2021 Adobe.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.adobe.testing.s3mock;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot 2.2+ does not include the default favicon.ico any more.
+ * This is needed to check if the S3 Mock is up (at least in our examples and some use-cases)
+ */
+@RestController
+public class FaviconController {
+  @RequestMapping("favicon.ico")
+  @ResponseBody
+  void favicon() {
+    // Method is intentionally empty.
+  }
+}
