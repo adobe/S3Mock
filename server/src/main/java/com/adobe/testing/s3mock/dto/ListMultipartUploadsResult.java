097207985d9d9d86ac34d1950111d9deb8e89723
Arne Franken <franken@adobe.com>
2021-07-05 17:45:45 +0200

    "Prefix" must be uppercase
    Not sure how deserialization of such a reply even worked.
    I'm guessing nobody uses common prefixes with that reply.

--- server/src/main/java/com/adobe/testing/s3mock/dto/ListMultipartUploadsResult.java
+++ server/src/main/java/com/adobe/testing/s3mock/dto/ListMultipartUploadsResult.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017-2019 Adobe.
+ *  Copyright 2017-2021 Adobe.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -112,7 +112,7 @@ public class ListMultipartUploadsResult {
   }
 
   public static class Prefix {
-    @JsonProperty
+    @JsonProperty("Prefix")
     private final String prefix;
 
     public Prefix(final String prefix) {
