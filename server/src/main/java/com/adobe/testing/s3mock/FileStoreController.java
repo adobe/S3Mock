a97e9956e7acf0553e7a71ad59d9b37047b66a4d
Matt Benson <gudnabrsam@gmail.com>
2022-02-01 14:47:17 -0600

    unused import

--- server/src/main/java/com/adobe/testing/s3mock/FileStoreController.java
+++ server/src/main/java/com/adobe/testing/s3mock/FileStoreController.java
@@ -119,7 +119,6 @@ import org.apache.commons.io.input.BoundedInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
