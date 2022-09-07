d5c00958252cd353ff638a3b438e50a9242fd082
Arne Franken <franken@adobe.com>
2022-09-07 19:29:27 +0200

    More thorough tests for ObjectTagging

--- integration-tests/src/test/kotlin/com/adobe/testing/s3mock/its/ObjectTaggingV2IT.kt
+++ integration-tests/src/test/kotlin/com/adobe/testing/s3mock/its/ObjectTaggingV2IT.kt
@@ -22,16 +22,61 @@ import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest
 import software.amazon.awssdk.services.s3.model.GetObjectTaggingRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectTaggingRequest
 import software.amazon.awssdk.services.s3.model.Tag
 import software.amazon.awssdk.services.s3.model.Tagging
 
-class TaggingResponsesV2IT : S3TestBase() {
+class ObjectTaggingV2IT : S3TestBase() {
 
-  /**
-   * Verify that tagging can be obtained and returns expected content.
-   */
   @Test
-  fun testObjectTaggingWithPutObjectRequest(testInfo: TestInfo) {
+  fun testGetObjectTagging_noTags(testInfo: TestInfo) {
+    val bucketName = bucketName(testInfo)
+    s3ClientV2!!.createBucket(CreateBucketRequest.builder().bucket(bucketName).build())
+    s3ClientV2!!.putObject(
+      { b: PutObjectRequest.Builder -> b.bucket(bucketName).key("foo") },
+      RequestBody.fromString("foo")
+    )
+
+    assertThat(s3ClientV2!!.getObjectTagging { b: GetObjectTaggingRequest.Builder ->
+      b.bucket(
+        bucketName
+      ).key("foo")
+    }
+      .tagSet())
+      .isEmpty()
+  }
+
+  @Test
+  fun testPutAndGetObjectTagging(testInfo: TestInfo) {
+    val bucketName = bucketName(testInfo)
+    val key = "foo"
+    s3ClientV2!!.createBucket(CreateBucketRequest.builder().bucket(bucketName).build())
+    val tag1 = Tag.builder().key("tag1").value("foo").build()
+    val tag2 = Tag.builder().key("tag2").value("bar").build()
+    s3ClientV2!!.putObject(
+      { b: PutObjectRequest.Builder -> b.bucket(bucketName).key(key) },
+      RequestBody.fromString("foo")
+    )
+
+    s3ClientV2!!.putObjectTagging(
+      PutObjectTaggingRequest.builder().bucket(bucketName).key(key)
+        .tagging(Tagging.builder().tagSet(tag1, tag2).build()).build()
+    )
+
+    assertThat(s3ClientV2!!.getObjectTagging { b: GetObjectTaggingRequest.Builder ->
+      b.bucket(
+        bucketName
+      ).key(key)
+    }
+      .tagSet())
+      .contains(
+        tag1,
+        tag2
+      )
+  }
+
+  @Test
+  fun testPutObjectAndGetObjectTagging_withTagging(testInfo: TestInfo) {
     val bucketName = bucketName(testInfo)
     s3ClientV2!!.createBucket(CreateBucketRequest.builder().bucket(bucketName).build())
     s3ClientV2!!.putObject(
@@ -52,7 +97,7 @@ class TaggingResponsesV2IT : S3TestBase() {
    * Verify that tagging with multiple tags can be obtained and returns expected content.
    */
   @Test
-  fun testObjectTaggingWithPutObjectRequest_multipleTags(testInfo: TestInfo) {
+  fun testPutObjectAndGetObjectTagging_multipleTags(testInfo: TestInfo) {
     val bucketName = bucketName(testInfo)
     s3ClientV2!!.createBucket(CreateBucketRequest.builder().bucket(bucketName).build())
     val tag1 = Tag.builder().key("tag1").value("foo").build()
@@ -61,7 +106,7 @@ class TaggingResponsesV2IT : S3TestBase() {
     s3ClientV2!!.putObject(
       { b: PutObjectRequest.Builder ->
         b.bucket(bucketName).key("multipleFoo")
-        .tagging(Tagging.builder().tagSet(tag1, tag2).build())
+          .tagging(Tagging.builder().tagSet(tag1, tag2).build())
       }, RequestBody.fromString("multipleFoo")
     )
 
