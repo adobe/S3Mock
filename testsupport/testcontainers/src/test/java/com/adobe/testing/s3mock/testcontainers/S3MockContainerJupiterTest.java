5af3e6ff5cfad795c6085df59820a976d7c66ac1
Arne Franken <franken@adobe.com>
2023-10-05 13:06:55 +0200

    Add repeated test * 1000
    This is to verify that a repeated restart of the S3Mock within
    TestContainers while using HTTPS to access it does not lead to
    intermittent failures.
    
    #1396

--- testsupport/testcontainers/src/test/java/com/adobe/testing/s3mock/testcontainers/S3MockContainerJupiterTest.java
+++ testsupport/testcontainers/src/test/java/com/adobe/testing/s3mock/testcontainers/S3MockContainerJupiterTest.java
@@ -16,9 +16,24 @@
 
 package com.adobe.testing.s3mock.testcontainers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 /**
  * This shows how to let JUnit 5 Jupiter start and stop the S3MockContainer.
@@ -41,4 +56,33 @@ class S3MockContainerJupiterTest extends S3MockContainerTestBase {
     var endpoint = s3Mock.getHttpsEndpoint();
     s3Client = createS3ClientV2(endpoint);
   }
+
+  /**
+   * Creates a bucket, stores a file, lists the bucket.
+   */
+  @RepeatedTest(1000)
+  void testListBucketsCreateBucketPutObjectAndListBucket(TestInfo testInfo) {
+    var bucketName = bucketName(testInfo);
+    var uploadFile = new File(UPLOAD_FILE_NAME);
+
+    var listBucketsResponse = s3Client.listBuckets();
+    assertThat(listBucketsResponse.buckets())
+        .hasSize(2)
+        .extracting(Bucket::name)
+        .containsExactlyInAnyOrder("bucket-a", "bucket-b");
+
+    s3Client.createBucket(CreateBucketRequest.builder().bucket(bucketName).build());
+    var putObjectResponse = s3Client.putObject(
+        PutObjectRequest.builder().bucket(bucketName).key(uploadFile.getName()).build(),
+        RequestBody.fromFile(uploadFile));
+
+    var listObjectsV2Response =
+        s3Client.listObjectsV2(ListObjectsV2Request.builder().bucket(bucketName).build());
+
+    assertThat(listObjectsV2Response.contents())
+        .hasSize(1)
+        .extracting(S3Object::key, S3Object::eTag)
+        .containsExactly(new Tuple(uploadFile.getName(), putObjectResponse.eTag()));
+  }
+
 }
