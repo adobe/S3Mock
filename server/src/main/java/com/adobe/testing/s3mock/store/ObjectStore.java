f755a9a8e3882f86311da62f41b26de00e1dc2dd
Arne Franken <coding@techotronic.de>
2024-05-28 23:04:06 +0200

    Store ACLs with ObjectMetadata
    This is a step to storing versions, as ACLs are stored by version in S3.

--- server/src/main/java/com/adobe/testing/s3mock/store/ObjectStore.java
+++ server/src/main/java/com/adobe/testing/s3mock/store/ObjectStore.java
@@ -32,7 +32,6 @@ import com.adobe.testing.s3mock.dto.StorageClass;
 import com.adobe.testing.s3mock.dto.Tag;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -54,7 +53,6 @@ import org.slf4j.LoggerFactory;
 public class ObjectStore extends StoreBase {
   private static final Logger LOG = LoggerFactory.getLogger(ObjectStore.class);
   private static final String META_FILE = "objectMetadata.json";
-  private static final String ACL_FILE = "objectAcl.json";
   private static final String DATA_FILE = "binaryData";
 
   /**
@@ -132,7 +130,8 @@ public class ObjectStore extends StoreBase {
           encryptionHeaders,
           checksumAlgorithm,
           checksum,
-          storageClass
+          storageClass,
+          null
       );
       writeMetafile(bucket, s3ObjectMetadata);
       return s3ObjectMetadata;
@@ -173,7 +172,8 @@ public class ObjectStore extends StoreBase {
           s3ObjectMetadata.encryptionHeaders(),
           s3ObjectMetadata.checksumAlgorithm(),
           s3ObjectMetadata.checksum(),
-          s3ObjectMetadata.storageClass()
+          s3ObjectMetadata.storageClass(),
+          s3ObjectMetadata.policy()
       ));
     }
   }
@@ -206,7 +206,8 @@ public class ObjectStore extends StoreBase {
           s3ObjectMetadata.encryptionHeaders(),
           s3ObjectMetadata.checksumAlgorithm(),
           s3ObjectMetadata.checksum(),
-          s3ObjectMetadata.storageClass()
+          s3ObjectMetadata.storageClass(),
+          s3ObjectMetadata.policy()
       ));
     }
   }
@@ -219,16 +220,38 @@ public class ObjectStore extends StoreBase {
    * @param policy the ACL.
    */
   public void storeAcl(BucketMetadata bucket, UUID id, AccessControlPolicy policy) {
-    writeAclFile(bucket, id, policy);
+    synchronized (lockStore.get(id)) {
+      var s3ObjectMetadata = getS3ObjectMetadata(bucket, id);
+      writeMetafile(bucket, new S3ObjectMetadata(
+              s3ObjectMetadata.id(),
+              s3ObjectMetadata.key(),
+              s3ObjectMetadata.size(),
+              s3ObjectMetadata.modificationDate(),
+              s3ObjectMetadata.etag(),
+              s3ObjectMetadata.contentType(),
+              s3ObjectMetadata.lastModified(),
+              s3ObjectMetadata.dataPath(),
+              s3ObjectMetadata.userMetadata(),
+              s3ObjectMetadata.tags(),
+              s3ObjectMetadata.legalHold(),
+              s3ObjectMetadata.retention(),
+              s3ObjectMetadata.owner(),
+              s3ObjectMetadata.storeHeaders(),
+              s3ObjectMetadata.encryptionHeaders(),
+              s3ObjectMetadata.checksumAlgorithm(),
+              s3ObjectMetadata.checksum(),
+              s3ObjectMetadata.storageClass(),
+              policy
+          )
+      );
+    }
   }
 
   public AccessControlPolicy readAcl(BucketMetadata bucket, UUID id) {
-    var policy = readAclFile(bucket, id);
-    if (policy == null) {
-      var s3ObjectMetadata = getS3ObjectMetadata(bucket, id);
-      return privateCannedAcl(s3ObjectMetadata.owner());
-    }
-    return policy;
+    var s3ObjectMetadata = getS3ObjectMetadata(bucket, id);
+    return s3ObjectMetadata.policy() == null
+        ? privateCannedAcl(s3ObjectMetadata.owner())
+        : s3ObjectMetadata.policy();
   }
 
   /**
@@ -259,7 +282,8 @@ public class ObjectStore extends StoreBase {
           s3ObjectMetadata.encryptionHeaders(),
           s3ObjectMetadata.checksumAlgorithm(),
           s3ObjectMetadata.checksum(),
-          s3ObjectMetadata.storageClass()
+          s3ObjectMetadata.storageClass(),
+          s3ObjectMetadata.policy()
       ));
     }
   }
@@ -374,7 +398,8 @@ public class ObjectStore extends StoreBase {
             ? sourceObject.encryptionHeaders() : encryptionHeaders,
         sourceObject.checksumAlgorithm(),
         sourceObject.checksum(),
-        storageClass != null ? storageClass : sourceObject.storageClass()
+        storageClass != null ? storageClass : sourceObject.storageClass(),
+        sourceObject.policy()
     ));
     return new CopyObjectResult(sourceObject.modificationDate(), sourceObject.etag());
   }
@@ -458,10 +483,6 @@ public class ObjectStore extends StoreBase {
     return Paths.get(getObjectFolderPath(bucket, id).toString(), META_FILE);
   }
 
-  private Path getAclFilePath(BucketMetadata bucket, UUID id) {
-    return Paths.get(getObjectFolderPath(bucket, id).toString(), ACL_FILE);
-  }
-
   private Path getDataFilePath(BucketMetadata bucket, UUID id) {
     return Paths.get(getObjectFolderPath(bucket, id).toString(), DATA_FILE);
   }
@@ -478,35 +499,6 @@ public class ObjectStore extends StoreBase {
       }
     } catch (IOException e) {
       throw new IllegalStateException("Could not write object metadata-file " + id, e);
-    }
-  }
-
-  private AccessControlPolicy readAclFile(BucketMetadata bucket, UUID id) {
-    try {
-      synchronized (lockStore.get(id)) {
-        var aclFile = getAclFilePath(bucket, id).toFile();
-        if (!aclFile.exists()) {
-          return null;
-        }
-        var toDeserialize = FileUtils.readFileToString(aclFile, Charset.defaultCharset());
-        return objectMapper.readValue(toDeserialize, AccessControlPolicy.class);
-      }
-    } catch (IOException e) {
-      throw new IllegalStateException("Could not read object acl-file " + id, e);
-    }
-  }
-
-  private void writeAclFile(BucketMetadata bucket, UUID id, AccessControlPolicy policy) {
-    try {
-      synchronized (lockStore.get(id)) {
-        var aclFile = getAclFilePath(bucket, id).toFile();
-        if (!retainFilesOnExit) {
-          aclFile.deleteOnExit();
-        }
-        FileUtils.write(aclFile, objectMapper.writeValueAsString(policy), Charset.defaultCharset());
-      }
-    } catch (IOException e) {
-      throw new IllegalStateException("Could not write object acl-file " + id, e);
     }
   }
 }
