7ba7dc7f59af189f86f305077e7f2696b61b92f1
Matthias Düsterhöft <m.duesterhoeft@googlemail.com>
2024-11-11 13:58:20 +0100

    Fix code style violation

--- server/src/main/java/com/adobe/testing/s3mock/S3MockApplication.java
+++ server/src/main/java/com/adobe/testing/s3mock/S3MockApplication.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package com.adobe.testing.s3mock;
 
 import static java.util.Collections.emptyMap;
