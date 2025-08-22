26471360944b990aebda29acac7d8075f3d5375b
Arne Franken <coding@techotronic.de>
2025-08-23 01:36:26 +0200

    Update to Spring-Boot 4.0.0-M2

--- server/src/test/kotlin/com/adobe/testing/s3mock/ObjectControllerTest.kt
+++ server/src/test/kotlin/com/adobe/testing/s3mock/ObjectControllerTest.kt
@@ -1132,7 +1132,7 @@ internal class ObjectControllerTest : BaseControllerTest() {
     ).thenReturn(s3ObjectMetadata)
 
     val headers = HttpHeaders().apply {
-      this[HttpHeaders.IF_MATCH] = listOf("\"etag-123\"")
+      this[HttpHeaders.IF_MATCH] = "\"etag-123\""
       this[AwsHttpHeaders.X_AMZ_SDK_CHECKSUM_ALGORITHM] = "CRC32"
       contentType = MediaType.APPLICATION_OCTET_STREAM
     }
