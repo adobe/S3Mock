b3b8a3aed35922e933cd758be47dd27b0375c5c4
Arne Franken <franken@adobe.com>
2025-11-13 17:52:12 -0800

    Update to Spring Boot 4.0.0-RC2

--- server/src/test/kotlin/com/adobe/testing/s3mock/controller/FaviconControllerTest.kt
+++ server/src/test/kotlin/com/adobe/testing/s3mock/controller/FaviconControllerTest.kt
@@ -18,7 +18,7 @@ package com.adobe.testing.s3mock.controller
 import com.adobe.testing.s3mock.store.KmsKeyStore
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
